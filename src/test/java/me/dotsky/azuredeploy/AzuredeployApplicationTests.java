e957068b31ea2c9b65c8e68d3bcde1ee78deb9a0
Sathish Jayapal <sjayapal@skminfotech.com>
2022-07-04 21:13:23 -0500

    Azure image- simple version

---
+++ src/test/java/me/dotsky/azuredeploy/AzuredeployApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dotsky.azuredeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzuredeployApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
